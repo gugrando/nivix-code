8d2ef85e0bd148e9016eb819b94b8010e074dc2b
gugrando <gustavo.vieira0909@gmail.com>
2025-08-11 17:50:50 -0300

    Latest_Version3.2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
           </p>
         </div>
         <div className="flex items-center justify-center gap-4">
-            <span className="text-[#F4B400]"><FaInstagram size={24} href="instagram.com/nivix.co" /></span>
+            <span className="text-[#F4B400]"><FaInstagram size={24} /></span>
             <span className="text-[#F4B400]"><FaFacebook size={24} /></span>
             <span className="text-[#F4B400]"><FaLinkedin size={24} /></span>
         </div>
